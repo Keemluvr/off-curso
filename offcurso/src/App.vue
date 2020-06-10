084ac451971ddf13e647963b709bec8d9f834167
Keila <keiladaianefernandes24@gmail.com>
2020-06-10 08:53:51 -0300

    :construction: created Header

--- offcurso/src/App.vue
+++ offcurso/src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header></Header>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from "@/components/Header.vue"
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  :root {
+    --primery-color: #234;
+    --secondary-color: #4b8;
+    --thirty-color: white;
+  }
 
-#nav {
-  padding: 30px;
-}
+  body {
+    margin: 0;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    color: var(--primery-color);
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  a {
+    font-weight: bold;
+    text-decoration: none;
+    color: var(--primery-color);
+  }
+
+ 
 </style>
